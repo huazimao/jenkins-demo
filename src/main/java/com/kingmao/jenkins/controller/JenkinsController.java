e6a911042b1a8334bc26a7f53c9edf24d3796d8d
qinxuan <yiqinxuan@chainup.com>
2020-05-19 14:32:52 +0800

    init jenkins demo code

---
+++ src/main/java/com/kingmao/jenkins/controller/JenkinsController.java
@@ -0,0 +1,17 @@
+package com.kingmao.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author QX
+ * @date 2020/5/19
+ */
+@RestController
+public class JenkinsController {
+
+    @GetMapping("/jenkins")
+    public String hello(){
+        return "Hello Jenkins !!!";
+    }
+}
