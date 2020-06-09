8efa8af5ac2aa3bb0bb177c5f98999f8685b9000
qinxuan <yiqinxuan@chainup.com>
2020-06-09 20:49:27 +0800

    up

--- src/main/java/com/kingmao/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/kingmao/jenkins/JenkinsDemoApplication.java
@@ -2,12 +2,23 @@ package com.kingmao.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+    /**
+     * springboot打包部署
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(JenkinsDemoApplication.class);
     }
 
 }
