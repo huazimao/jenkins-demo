1e1242c9e428835aae555fcd54756842b811dbe0
huazimao <laisigou321@163.com>
2020-06-09 23:13:04 +0800

    2333

--- src/main/java/com/kingmao/jenkins/controller/JenkinsController.java
+++ src/main/java/com/kingmao/jenkins/controller/JenkinsController.java
@@ -12,6 +12,6 @@ public class JenkinsController {
 
     @GetMapping("/jenkins")
     public String hello(){
-        return "Hello Jenkins 23333!!!";
+        return "Hello Jenkins 456!!!";
     }
 }
